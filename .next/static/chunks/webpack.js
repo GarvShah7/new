4d954bb170174e13468bc0659229f94cde213b55
GarvShah7 <garvcodage@gmail.com>
2023-07-04 12:09:19 +0530

    initial

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "086826d1f9d0b8af"; }
+/******/ 		__webpack_require__.h = function() { return "941036046976ec9c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,11 +1,18 @@
-import React, { useState, memo } from "react";
+import {
+  FooterFragment,
+  HeaderFragment,
+  HireDedicatedPageTypesFragment,
+  HomeNavServicesFragment,
+  ServiceTypeFragment,
+} from "../../app/api/generated/graphql/graphql";
 import clsx from "clsx";
+import { useState } from "react";
+import React from "react";
 import dynamic from "next/dynamic";
-
 const NavOpen = dynamic(() => import("./NavOpen"));
 const NavClose = dynamic(() => import("./NavClose"));
 
-type HeaderProps = {
+export type HeaderProps = {
   block?: HeaderFragment;
   footer?: FooterFragment;
   allServicesTypes?: ServiceTypeFragment[];
@@ -14,44 +21,43 @@ type HeaderProps = {
   hireDedicatedPages?: HomeNavServicesFragment[];
 };
 
-const Header: React.FC<HeaderProps> = memo(
-  ({
-    block,
-    footer,
-    allServicesTypes,
-    servicesPage,
-    hireDedicatedType,
-    hireDedicatedPages,
-  }) => {
-    const { sectionTheme } = block || {};
-    const [navOpen, setNavOpen] = useState(false);
+const Header: React.FC<HeaderProps> = ({
+  block,
+  footer,
+  allServicesTypes,
+  servicesPage,
+  hireDedicatedType,
+  hireDedicatedPages,
+}) => {
+  const { sectionTheme } = block || {};
+  const [navOpen, setNavOpen] = useState(false);
+  const openNav = (val: boolean) => {
+    if (val) {
+      document.body.classList.add("modal-open");
+    } else {
+      document.body.classList.remove("modal-open");
+    }
+    setNavOpen(val);
+  };
+  return (
+    <div className="block bg-black">
+      <header className={clsx("headerSection", sectionTheme ?? "bg-white")}>
+        {!navOpen && (
+          <NavOpen
+            onClickOpenNav={openNav}
+            hireDedicatedPages={hireDedicatedPages}
+            hireDedicatedType={hireDedicatedType}
+            servicesPage={servicesPage}
+            allServicesTypes={allServicesTypes}
+            block={block}
+          />
+        )}
+        {navOpen && (
+          <NavClose block={block} onClickOpenNav={openNav} footer={footer} />
+        )}
+      </header>
+    </div>
+  );
+};
 
-    const openNav = (val: boolean) => {
-      setNavOpen(val);
-    };
-
-    const bodyClass = navOpen ? "modal-open" : "";
-
-    return (
-      <div className="block bg-black">
-        <header className={clsx("headerSection", sectionTheme ?? "bg-white")}>
-          {!navOpen && (
-            <NavOpen
-              onClickOpenNav={openNav}
-              hireDedicatedPages={hireDedicatedPages}
-              hireDedicatedType={hireDedicatedType}
-              servicesPage={servicesPage}
-              allServicesTypes={allServicesTypes}
-              block={block}
-            />
-          )}
-          {navOpen && (
-            <NavClose block={block} onClickOpenNav={openNav} footer={footer} />
-          )}
-        </header>
-      </div>
-    );
-  }
-);
-
-export default Header;
+export default React.memo(Header);
