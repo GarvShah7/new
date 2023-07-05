77f9c30f648840e026c6cbb433e12aa9afdc05f1
GarvShah7 <garvcodage@gmail.com>
2023-07-05 10:34:04 +0530

    s

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cb500fd0ce800ced"; }
+/******/ 		__webpack_require__.h = function() { return "1e8733da7ee9e2fb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Header/NavOpen/index.tsx
+++ components/Header/NavOpen/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useMemo } from "react";
+import React, { useState, useCallback } from "react";
 import {
   HeaderFragment,
   HireDedicatedPageTypesFragment,
@@ -23,149 +23,153 @@ export type NavOpenProps = {
   onClickOpenNav: (val?: boolean) => void;
 };
 
-const NavOpen: React.FC<NavOpenProps> = ({
-  allServicesTypes,
-  block,
-  servicesPage,
-  hireDedicatedType,
-  hireDedicatedPages,
-  onClickOpenNav,
-}) => {
-  const { navItems, logo } = block || {};
+const NavOpen: React.FC<NavOpenProps> = React.memo(
+  ({
+    allServicesTypes,
+    block,
+    servicesPage,
+    hireDedicatedType,
+    hireDedicatedPages,
+    onClickOpenNav,
+  }) => {
+    const { navItems, logo } = block || {};
 
-  const extractedSubItems = useMemo(() => {
-    return allServicesTypes?.map((i) => {
-      const data =
-        servicesPage &&
-        servicesPage.filter((item) => item?.service_types?.label === i?.label);
-      return { label: i?.label, data: data };
-    });
-  }, [allServicesTypes, servicesPage]);
+    const extractedSubItems = React.useMemo(() => {
+      return allServicesTypes?.map((i) => {
+        const data =
+          servicesPage &&
+          servicesPage.filter(
+            (item) => item?.service_types?.label === i?.label
+          );
+        return { label: i?.label, data: data };
+      });
+    }, [allServicesTypes, servicesPage]);
 
-  const extractedSubItemsOfHireDedicated = useMemo(() => {
-    return hireDedicatedType?.map((item) => {
-      const data =
-        hireDedicatedPages &&
-        hireDedicatedPages?.filter(
-          (i) => i?.hire_dedicated_page_types?.label === item?.label
-        );
-      return { label: item?.label, data: data };
-    });
-  }, [hireDedicatedType, hireDedicatedPages]);
+    const extractedSubItemsOfHireDedicated = React.useMemo(() => {
+      return hireDedicatedType?.map((item) => {
+        const data =
+          hireDedicatedPages &&
+          hireDedicatedPages?.filter(
+            (i) => i?.hire_dedicated_page_types?.label === item?.label
+          );
+        return { label: item?.label, data: data };
+      });
+    }, [hireDedicatedType, hireDedicatedPages]);
 
-  const [isLinkActive, setLinkActive] = useState(false);
-  const handleClickOpenNav = useCallback(() => {
-    onClickOpenNav(false);
-  }, [onClickOpenNav]);
+    const [isLinkActive, setLinkActive] = useState(false);
+    const handleClickOpenNav = useCallback(() => {
+      onClickOpenNav(false);
+    }, [onClickOpenNav]);
 
-  const handleClickToggle = useCallback(() => {
-    onClickOpenNav(true);
-  }, [onClickOpenNav]);
+    const handleClickToggle = useCallback(() => {
+      onClickOpenNav(true);
+    }, [onClickOpenNav]);
 
-  const NavItems = useCallback(
-    (item: NavbarLinksFragment) => {
-      const { navLabel, navLink } = item || {};
-      return (
-        <React.Fragment key={navLabel}>
-          <li className="navItem ml-[3.125em]" onClick={handleClickOpenNav}>
-            <Link
-              href={navLink as string}
-              className="text-[1.125em] text-black font-[500] hover:text-[#000ee6]"
-            >
-              {navLabel}
-            </Link>
-          </li>
-        </React.Fragment>
-      );
-    },
-    [handleClickOpenNav]
-  );
-
-  return (
-    <div className="max-w-[1440px] w-[90%] mx-auto">
-      <div className="py-[3.125em] flex justify-between items-center z-[999]">
-        <div className="logo">
-          <Link href="/">{logo && <Image block={logo} />}</Link>
-        </div>
-        <nav className="navigation">
-          <ul className="flex items-center font-Montserrat">
-            {/* dropdown */}
-            <li className="navItem dropdown">
+    const NavItems = useCallback(
+      (item: NavbarLinksFragment) => {
+        const { navLabel, navLink } = item || {};
+        return (
+          <React.Fragment key={navLabel}>
+            <li className="navItem ml-[3.125em]" onClick={handleClickOpenNav}>
               <Link
-                href="/services"
-                target="_self"
+                href={navLink as string}
                 className="text-[1.125em] text-black font-[500] hover:text-[#000ee6]"
               >
-                Services
+                {navLabel}
               </Link>
-              <div
-                className={`dropdown-content absolute ${
-                  isLinkActive ? "none" : "block"
-                }`}
-              >
+            </li>
+          </React.Fragment>
+        );
+      },
+      [handleClickOpenNav]
+    );
+
+    return (
+      <div className="max-w-[1440px] w-[90%] mx-auto">
+        <div className="py-[3.125em] flex justify-between items-center z-[999]">
+          <div className="logo">
+            <Link href="/">{logo && <Image block={logo} />}</Link>
+          </div>
+          <nav className="navigation">
+            <ul className="flex items-center font-Montserrat">
+              {/* dropdown */}
+              <li className="navItem dropdown">
+                <Link
+                  href="/services"
+                  target="_self"
+                  className="text-[1.125em] text-black font-[500] hover:text-[#000ee6]"
+                >
+                  Services
+                </Link>
                 <div
-                  className={`dropdownGrid col-${
-                    extractedSubItems && extractedSubItems?.length - 1
+                  className={`dropdown-content absolute ${
+                    isLinkActive ? "none" : "block"
                   }`}
                 >
-                  {extractedSubItems
-                    ?.filter((i) => i?.data?.length !== 0)
-                    ?.map((i) => {
+                  <div
+                    className={`dropdownGrid col-${
+                      extractedSubItems && extractedSubItems?.length - 1
+                    }`}
+                  >
+                    {extractedSubItems
+                      ?.filter((i) => i?.data?.length !== 0)
+                      ?.map((i) => {
+                        return (
+                          <div className="dropdownBox" key={i.label}>
+                            {i && <ServicesNavbar block={i} />}
+                          </div>
+                        );
+                      })}
+                  </div>
+                </div>
+              </li>
+              {/* ====== */}
+              <li className="navItem dropdown hireDeveloperIte ml-[3.125em]">
+                <Link
+                  href="/services"
+                  target="_self"
+                  className="text-[1.125em] text-black font-[500] hover:text-[#000ee6]"
+                >
+                  Hire Developers
+                </Link>
+                <div
+                  className={`dropdown-content absolute ${
+                    isLinkActive ? "none" : "block"
+                  }`}
+                >
+                  <div className="dropdownGrid col-1">
+                    {extractedSubItemsOfHireDedicated?.map((i) => {
                       return (
                         <div className="dropdownBox" key={i.label}>
                           {i && <ServicesNavbar block={i} />}
                         </div>
                       );
                     })}
+                  </div>
                 </div>
-              </div>
-            </li>
-            {/* ====== */}
-            <li className="navItem dropdown hireDeveloperIte ml-[3.125em]">
-              <Link
-                href="/services"
-                target="_self"
-                className="text-[1.125em] text-black font-[500] hover:text-[#000ee6]"
+              </li>
+              {/* dropdown */}
+              {navItems?.map((item: NavbarLinksFragment) => {
+                return (
+                  <React.Fragment key={item.navLabel}>
+                    {NavItems(item)}
+                  </React.Fragment>
+                );
+              })}
+              <li
+                className="navItem withToggle ml-[4em]"
+                onClick={handleClickToggle}
               >
-                Hire Developers
-              </Link>
-              <div
-                className={`dropdown-content absolute ${
-                  isLinkActive ? "none" : "block"
-                }`}
-              >
-                <div className="dropdownGrid col-1">
-                  {extractedSubItemsOfHireDedicated?.map((i) => {
-                    return (
-                      <div className="dropdownBox" key={i.label}>
-                        {i && <ServicesNavbar block={i} />}
-                      </div>
-                    );
-                  })}
+                <div className="toggleIcon">
+                  <NextImage src={toggle} alt="toggle" className="w-auto" />
                 </div>
-              </div>
-            </li>
-            {/* dropdown */}
-            {navItems?.map((item: NavbarLinksFragment) => {
-              return (
-                <React.Fragment key={item.navLabel}>
-                  {NavItems(item)}
-                </React.Fragment>
-              );
-            })}
-            <li
-              className="navItem withToggle ml-[4em]"
-              onClick={handleClickToggle}
-            >
-              <div className="toggleIcon">
-                <NextImage src={toggle} alt="toggle" className="w-auto" />
-              </div>
-            </li>
-          </ul>
-        </nav>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
-export default React.memo(NavOpen);
+export default NavOpen;
